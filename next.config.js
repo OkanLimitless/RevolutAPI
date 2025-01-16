173c85ff45902cf7ac58629d214a9474916a26ca
OkanLimitless <okangalatasaray001@gmail.com>
2025-01-16 20:38:11 +0100

    Update Revolut API configuration with new access and refresh tokens in next.config.js. This change replaces the hardcoded token values to facilitate integration testing while ensuring compliance with the latest API specifications.

--- next.config.js
+++ next.config.js
@@ -18,9 +18,9 @@ module.exports = {
         REVOLUT_REDIRECT_URI: 'https://api.diamondskymarketing.nl/api/revolut/oauth/callback',
         REVOLUT_KEY_SIZE: 2048,
         REVOLUT_CLIENT_ID: 'vdt7F3GzdffAZ_EWW9VFjZtLC-qOttZpcVgB-RZISiE',
-        REVOLUT_ACCESS_TOKEN: 'oa_prod_nUiDq8rNgPjSw3lVyD1NObdLVjTgbSFUCkag7D8FQpE',
-        REVOLUT_REFRESH_TOKEN: 'oa_prod_vbD568x-AvJWTTa1BNdM3Irqsq0O4GLo90Zh7jtzTHI',
-        REVOLUT_TOKEN_EXPIRES: '1737057193703',
+        REVOLUT_ACCESS_TOKEN: 'oa_prod_knWZ4V5XROFACtV-P2xGzIFuPU58qoCZVXpguuSjQp0',
+        REVOLUT_REFRESH_TOKEN: 'oa_prod_uJqHttLfAHfZKsAbOzRAGYqx3Lfo3OY5AJZMU7-mQLQ',
+        REVOLUT_TOKEN_EXPIRES: '1737059823106',
         REVOLUT_SCOPES: 'READ,WRITE,PAY'
     }
 }; 
